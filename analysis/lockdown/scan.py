1213b9b711ad951d347cdd122d4098250060ef52
Thomas House <thomas.house@manchester.ac.uk>
2020-04-07 17:36:33 +0100

    Finished lockdown scenario sweep spec draft

--- analysis/lockdown/scan.py
+++ analysis/lockdown/scan.py
@@ -16,10 +16,7 @@ if __name__ == '__main__':
     lockdown_durations = array([21.0, 90.0, 180.0])
     SAPi_range = array([0.4, 0.6, 0.8, 0.9])
 
-    msg = 'Done global reduction range {0:d} of {1:d};
-    compliance range {2:d} of {3:d};
-    duration range {4:d} of {5:d};
-    SAP range {6:d} of {7:d}.'
+    msg = 'Done global reduction range {0:d} of {1:d}; compliance range {2:d} of {3:d}; duration range {4:d} of {5:d}; SAP range {6:d} of {7:d}.'
     results = []
 
     for ig, g in enumerate(globred_range):
